5aa1cce92cea77b18ee8d63d12faf8f62fbc5a3a
stacy <stacydn23@gmail.com>
2020-10-26 21:01:17 -0400

    commit tree c32d0a0b29d7b0bab46c42ebc8cb9ae6f8b91db7

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  new_numbers = [ ]
+  numbers.each do |number|
+    new_numbers << (number ** 2)
+end
+return new_numbers
 end
